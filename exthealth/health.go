862f0fabb8557b2b72b5149d9aa5d2fa89332784
Daniel Reuter <daniel@steadybit.com>
2023-04-06 11:25:42 +0200

    feat: default readiness is true, use atomic variable for readiness

--- exthealth/health.go
+++ exthealth/health.go
@@ -6,10 +6,11 @@ package exthealth
 import (
 	"github.com/steadybit/extension-kit/exthttp"
 	"net/http"
+	"sync/atomic"
 )
 
 var (
-	isReady = false
+	isReady int32 = 1
 )
 
 // AddLivenessProbe registers an HTTP handler for the liveness probe. The liveness probe reports HTTP 200 as soon as the HTTP server is up and running.
@@ -19,10 +20,10 @@ func AddLivenessProbe() {
 	})
 }
 
-// AddReadinessProbe registers an HTTP handler for the readiness probe. The readiness probe reports an error (HTTP 503) until the SetReady function is called with true.
+// AddReadinessProbe registers an HTTP handler for the readiness probe. The readiness probe reports an error (HTTP 503) when the SetReady function is called with false. Default readiness state is true.
 func AddReadinessProbe() {
 	exthttp.RegisterHttpHandler("/health/readiness", func(w http.ResponseWriter, r *http.Request, body []byte) {
-		if isReady {
+		if atomic.LoadInt32(&isReady) == 1 {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
@@ -30,7 +31,17 @@ func AddReadinessProbe() {
 	})
 }
 
+// AddProbes registers HTTP handlers for the liveness and readiness probes.
+func AddProbes() {
+	AddLivenessProbe()
+	AddReadinessProbe()
+}
+
 // SetReady sets the readiness state of the service. If the service is not ready the readiness probe will report an error.
 func SetReady(ready bool) {
-	isReady = ready
+	if ready {
+		atomic.StoreInt32(&isReady, 1)
+	} else {
+		atomic.StoreInt32(&isReady, 0)
+	}
 }
